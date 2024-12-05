e9c57ede9df8673636bdadd7528c1875d09f06b3
LiamCun1 <77689807+LiamCun1@users.noreply.github.com>
2024-12-04 21:09:25 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# AWS-Proj-Refined
+# AWS-Proj
+
+ # PENN Lambda Project  
+
+ The purpose of this project is to create a Lambda function within AWS to check a new object added to an s3 bucket and grab the objects metadata if it is an image.  
+
+ ## Installation  
+
+ Use the package manager [pip](https://pip.pypa.io/en/stable) to install dependencies.  
+
+```bash
+pip install boto3; moto; psycopg2; PIL
+```  
+
+## Usage  
+
+Clone this repository
+
+```bash
+git clone https://github.com/LiamCun1/AWS-Proj.git
+cd AWS-Proj
+```  
+
+Replace variable holders in config.ini to reflect your own environment.
+
+## Overview of Methods  
+
+- Utilizing configparser to securely connect to database.
+- Initialize s3 client and navigate to proper bucket.
+- If an object does not begin with \Image do not continue with the function.
+- metadata: Uses head_object to pull metadata from object in bucket to utilize for file_type and file_size.
+- image_dimensions_data: Uses get_object to retrieve the object from the bucket.
+- image_size: Uses Pillow to open the image and retrieve the width and height of it.
+- datadict: Dictionary containing relevant data from the object to use in the database.
+- create_table: Creates a new table s3_data_collection if it does not already exist.
+- insert_query: Creates the query to input the data into the created table.
+- DB_connection.commit: Commit changes made to database.
+- DB_cursor.close: Close cursor so further changes may not be made to database.
---
+++ main.py
@@ -0,0 +1,73 @@
+import boto3
+import psycopg2
+from PIL import Image
+import configparser
+
+config = configparser.RawConfigParser()
+config.read('config.ini')
+
+DB_connection = psycopg2.connect(
+    database = config.get('DB_DATABASE'),
+    user = config.get('DB_USER'),
+    password = config.get('DB_PASSWORD'),
+    host = config.get('DB_HOST'),
+    port = config.get('DB_PORT')
+)
+
+DB_cursor = DB_connection.cursor()
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+
+        if not object_key.startswith('\image'):
+            continue
+
+
+        metadata = s3.head_object(Bucket=bucket_name, Key=object_key)
+
+        file_type = metadata['ContentType']
+
+        file_size = metadata['ContentLength']
+
+        image_dimensions_data = s3.get_object(Bucket=bucket_name, Key=object_key)['Body']
+        image_size = Image.open(image_dimensions_data)
+        width, height = image_size.size
+
+        datadict = {
+            'File_Type':file_type,
+            'File_Size':file_size,
+            'width':width,
+            'height':height,
+            'File_Name':object_key
+        }
+
+        create_table = (
+            '''CREATE TABLE IF TABLE NOT EXISTS s3_data_collection (
+            File_Type VARCHAR(255),
+            File_Size INT,
+            width INT,
+            height INT,
+            File_Name VARCHAR(255))'''
+        )
+        DB_cursor.execute(create_table)
+
+        insert_query = (
+            '''INSERT INTO s3_data_collection(File_Type, File_Size, width, height, File_Name)
+            VALUES (%s, %s, %s, %s, %s)'''
+        )
+
+        DB_cursor.execute(insert_query, (
+            datadict['File_Type'],
+            datadict['FIle_Size'],
+            datadict['width'],
+            datadict['height'],
+            datadict['File_Name']
+        ))
+    DB_connection.commit()
+    DB_cursor.close()
+
+    
+
